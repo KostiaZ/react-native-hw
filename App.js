32d98e18dd2d08f494d2f27ba959709605db7610
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-04-08 21:00:19 +0300

    add registration form

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, Image, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.img} source={require("./assets/img/bg.jpg")}></Image>
+
+      <View style={styles.formContainer}>
+        <View style={styles.form}>
+          <Text style={styles.title}>Регистрация</Text>
+
+          <TextInput
+            style={styles.input}
+            placeholder="Логин"
+            placeholderTextColor={"#BDBDBD"}
+          ></TextInput>
+
+          <TextInput
+            style={styles.input}
+            placeholder="Адрес электронной почты"
+            placeholderTextColor={"#BDBDBD"}
+            autoComplete="email"
+            keyboardType="email-address"
+          ></TextInput>
+
+          <TextInput
+            style={styles.input}
+            placeholder="Пароль"
+            placeholderTextColor={"#BDBDBD"}
+          ></TextInput>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +39,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+  },
+  img: {
+    position: "absolute",
+    width: "100%",
+  },
+  formContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  form: {
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 30,
+    color: "#212121",
+    paddingBottom: 33,
+  },
+  input: {
+    marginHorizontal: 16,
+    padding: 15,
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    marginBottom: 16,
   },
 });
---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,13 @@
+import { View, StyleSheet, Text } from "react-native";
+
+export default RegistrationScreen = () => {
+  <View style={styles.conntainer}>
+    <Text>Jjkjkbsdj</Text>
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  conntainer: {
+    flex: 1,
+  },
+});
