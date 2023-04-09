940b379a8b5c1393ac7723c3c849ac579ec6e26e
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-04-10 00:08:11 +0300

    add styles

--- App.js
+++ App.js
@@ -1,35 +1,70 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+
   return (
     <View style={styles.container}>
       <Image style={styles.img} source={require("./assets/img/bg.jpg")}></Image>
-
       <View style={styles.formContainer}>
-        <View style={styles.form}>
-          <Text style={styles.title}>Регистрация</Text>
-
-          <TextInput
-            style={styles.input}
-            placeholder="Логин"
-            placeholderTextColor={"#BDBDBD"}
-          ></TextInput>
-
-          <TextInput
-            style={styles.input}
-            placeholder="Адрес электронной почты"
-            placeholderTextColor={"#BDBDBD"}
-            autoComplete="email"
-            keyboardType="email-address"
-          ></TextInput>
-
-          <TextInput
-            style={styles.input}
-            placeholder="Пароль"
-            placeholderTextColor={"#BDBDBD"}
-          ></TextInput>
-        </View>
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : ""}>
+          <View style={styles.form}>
+            <View style={styles.photo}>
+              <TouchableOpacity activeOpacity={0.7}>
+                <Image source={require("./assets/img/add.png")}></Image>
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.title}>Регистрация</Text>
+            <TextInput
+              style={{
+                ...styles.input,
+                borderColor: isKeyboardOpen ? "#E8E8E8" : "#FF6C00",
+              }}
+              placeholder="Логин"
+              placeholderTextColor={"#BDBDBD"}
+              onFocus={() => setIsKeyboardOpen(false)}
+            ></TextInput>
+            <TextInput
+              style={{
+                ...styles.input,
+                borderColor: isKeyboardOpen ? "#E8E8E8" : "#FF6C00",
+              }}
+              placeholder="Адрес электронной почты"
+              placeholderTextColor={"#BDBDBD"}
+              autoComplete="email"
+              keyboardType="email-address"
+              onFocus={() => setIsKeyboardOpen(false)}
+            ></TextInput>
+            <TextInput
+              style={{
+                ...styles.input,
+                borderColor: isKeyboardOpen ? "#E8E8E8" : "#FF6C00",
+              }}
+              placeholder="Пароль"
+              placeholderTextColor={"#BDBDBD"}
+              secureTextEntry={true}
+              onFocus={() => setIsKeyboardOpen(false)}
+            ></TextInput>
+            <TouchableOpacity>
+              <Text style={styles.btnShow} activeOpacity={0.7}>
+                Показать
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+              <Text style={styles.buttonTitle}>Зарегистрироваться</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
       </View>
     </View>
   );
@@ -54,6 +89,16 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
   },
+  photo: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: 120,
+    height: 120,
+    backgroundColor: "#F6F6F6",
+    marginBottom: 32,
+    borderRadius: 16,
+    top: -60,
+  },
   title: {
     textAlign: "center",
     fontSize: 30,
@@ -65,8 +110,29 @@ const styles = StyleSheet.create({
     padding: 15,
     backgroundColor: "#F6F6F6",
     borderWidth: 1,
-    borderColor: "#E8E8E8",
+    // borderColor: "#E8E8E8",
     borderRadius: 8,
     marginBottom: 16,
+  },
+  btnShow: {
+    color: "#1B4371",
+    fontSize: 16,
+    position: "absolute",
+    right: 32,
+    top: -56,
+  },
+  button: {
+    marginHorizontal: 16,
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    padding: 16,
+    marginTop: 43,
+    marginBottom: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonTitle: {
+    fontSize: 16,
+    color: "#fff",
   },
 });
